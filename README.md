31eafdded18087c7058b9d6893060f612d794394
Donny Rendi <donny.rendi@yahoo.co.id>
2023-08-21 16:44:45 +0700

    adding readme, react portal)

---
+++ README.md
@@ -0,0 +1,11 @@
+how to run this project
+
+npm start
+
+this project involve:
+
+components
+conditional rendering
+deleting rendered name list on click
+useState
+React Portals
--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -1,23 +1,31 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Button from '../Button/Button'
 
 function ErrorModal(props) {
   const setCloseModal = () => {
     props.closeModal()
   }
-  
+
+
+
   return (
-    <div className="error-overlay" onClick={setCloseModal}>
-      <div className="error-modal">
-        <div className='head'>
-          <h5>Invalid Input</h5>
+    <>
+      {ReactDOM.createPortal(<div className="error-overlay" onClick={setCloseModal}>
+        <div className="error-modal">
+          <div className='head'>
+            <h5>Invalid Input</h5>
+          </div>
+          <div className='content'>
+            <span>{props.content}</span>
+            <Button onClick={setCloseModal}>Okay</Button>
+          </div>
         </div>
-        <div className='content'>
-          <span>{props.content}</span>
-          <Button onClick={setCloseModal}>Okay</Button>
-        </div>
-      </div>
-    </div>
+      </div>,
+        document.getElementById('modal-root')
+      )}
+    </>
+
   )
 }
 
